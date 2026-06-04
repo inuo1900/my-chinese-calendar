e62a92294f2f51c31575bb18bbfb7620a9d5afa6
inuo1900 <inuo.1900@gmail.com>
2026-06-04 09:51:07 +0800

    Update split.rb

--- split.rb
+++ split.rb
@@ -1,15 +1,16 @@
-# split_clean.rb
 require 'icalendar'
+require 'open-uri'
 
-input_file = 'original_calendar.ics'
-holiday_file = 'holiday_green.ics'
-workday_file = 'workday_red.ics'
-festival_file = 'festival_blue.ics'
+# 下载原始日历
+url = 'https://calendars.icloud.com/holidays/cn_zh.ics'
+File.write('original_calendar.ics', URI.open(url).read)
 
-cal_file = File.open(input_file)
+# 解析日历
+cal_file = File.open('original_calendar.ics')
 cals = Icalendar::Calendar.parse(cal_file)
 cal = cals.first
 
+# 创建三个空日历
 holiday_cal = Icalendar::Calendar.new
 workday_cal = Icalendar::Calendar.new
 festival_cal = Icalendar::Calendar.new
@@ -17,7 +18,6 @@ festival_cal = Icalendar::Calendar.new
 cal.events.each do |event|
   summary = event.summary.to_s
   if summary.include?('休')
-    # 复制事件，但修改标题为 "休"
     new_event = Icalendar::Event.new
     new_event.dtstart = event.dtstart
     new_event.dtend = event.dtend
@@ -34,15 +34,13 @@ cal.events.each do |event|
     new_event.uid = event.uid
     workday_cal.add_event(new_event)
   elsif summary.include?('节') || summary == '除夕'
-    # 保留原节日名称
     festival_cal.add_event(event)
   end
 end
 
-# 写入文件
-File.open(holiday_file, 'w') { |f| f.write(holiday_cal.to_ical) }
-File.open(workday_file, 'w') { |f| f.write(workday_cal.to_ical) }
-File.open(festival_file, 'w') { |f| f.write(festival_cal.to_ical) }
+File.open('holiday_green.ics', 'w') { |f| f.write(holiday_cal.to_ical) }
+File.open('workday_red.ics', 'w') { |f| f.write(workday_cal.to_ical) }
+File.open('festival_blue.ics', 'w') { |f| f.write(festival_cal.to_ical) }
 
 puts "已生成: holiday_green.ics (事件标题为‘休’)"
 puts "已生成: workday_red.ics (事件标题为‘班’)"
